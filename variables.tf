8b3684470b28df27b085639567196cf6797ba0ec
kmobl <moblkourosh@gmail.com>
2023-09-13 12:23:38 -0500

    fixing partition language

--- variables.tf
+++ variables.tf
@@ -8,20 +8,6 @@ variable "aws_region" {
   type        = string
 }
 
-
-variable "is_gov" {
-  description = "Whether or not resources will be deployed in a govcloud region"
-  type        = bool
-}
-
-locals {
-  gov = var.is_gov ? "aws-us-gov" : "aws"
-}
-
-locals {
-  partition = var.is_gov ? "aws-us-gov" : "aws"
-}
-
 variable "account_number" {
   description = "The AWS account number resources are being deployed into"
   type        = string
